d30ff9efb05a8f9f913401aaff7f726729d1d439
Noah Finestone <noahfinestone@Noahs-MacBook-Pro.local>
2023-04-03 13:36:29 -0400

    few changes

--- Programming-Paradigms/Recursion/palindrome.py
+++ Programming-Paradigms/Recursion/palindrome.py
@@ -1,13 +1,3 @@
-# def palin(n: int, l:int, r : int) -> bool:
-#     if l >= r:
-#         return True
-#     if str(n)[l] != str(n)[r]:
-#         return False
-#     l += 1 
-#     r -= 1 
-#     return palin(n,l,r)
-# print(palin(22, 0, 1))
-
 def is_palindrome(left: int, right: int, num: int) -> bool:
     if left >= right:
         return True
@@ -18,4 +8,14 @@ def is_palindrome(left: int, right: int, num: int) -> bool:
 
 num = 12321
 print(is_palindrome(0, len(str(num))-1, num))
+
+# def palin(n: int, l:int, r : int) -> bool:
+#     if l >= r:
+#         return True
+#     if str(n)[l] != str(n)[r]:
+#         return False
+#     l += 1 
+#     r -= 1 
+#     return palin(n,l,r)
+# print(palin(22, 0, 1))
 
